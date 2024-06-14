04459d3c076905ed92f1c0e056e713de3fc580c8
nieshuo <694891769@qq.com>
2024-06-14 17:09:59 +0800

    1丶完成至postgres连接

---
+++ EShopMicroservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        :ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Business logic to create a product
+            // 1.create Product entity from command object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            // 2.save to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+            // 3.return CreateProductResult result
+            return new CreateProductResult(product.Id);
+             
+        }
+    }
+}
--- EShopMicroservices/Services/Catalog/Catalog.API/Program.cs
+++ EShopMicroservices/Services/Catalog/Catalog.API/Program.cs
@@ -2,8 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+    // options.AutoCreateSchemaObjects = Weasel.Core.AutoCreate.All;
+}).UseLightweightSessions();
+
+
 var app = builder.Build();
- 
+
 // Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
