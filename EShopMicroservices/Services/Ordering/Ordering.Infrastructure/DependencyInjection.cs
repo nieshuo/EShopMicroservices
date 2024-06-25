7f63facd66639b99e37be58e576a1fe23a13daba
nieshuo <694891769@qq.com>
2024-06-25 17:09:34 +0800

    1丶完成订单的基础文件

---
+++ EShopMicroservices/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices
+            (this IServiceCollection services,IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+
+            // Add Services to the container.
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //{
+            //    options.UseSqlServer(connectionString);
+            //});
+
+            //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
