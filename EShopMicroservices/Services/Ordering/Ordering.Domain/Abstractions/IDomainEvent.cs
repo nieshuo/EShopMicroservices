e2966f7ffdb459d432dd64a62664b973c7a13b32
nieshuo <694891769@qq.com>
2024-06-28 17:23:22 +0800

    1丶提交领域里的实体，值对象，集域，枚举

---
+++ EShopMicroservices/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstractions
+{
+    public class IDomainEvent:INotification
+    {
+        Guid EventId = Guid.NewGuid();
+        public DateTime OccurredOn => DateTime.Now;
+
+        // 从当前程序集中获取事件类型的限定名称
+        public string EventType => GetType().AssemblyQualifiedName;
+    }
+}
