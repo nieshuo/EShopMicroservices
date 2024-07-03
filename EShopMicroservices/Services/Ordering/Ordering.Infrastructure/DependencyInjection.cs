43491e70b60c82ef6933aa8c1eb6241dc764b477
nieshuo <694891769@qq.com>
2024-07-03 17:06:12 +0800

    1丶完成order的数据库连接

--- EShopMicroservices/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ EShopMicroservices/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ordering.Infrastructure
 {
@@ -16,12 +11,12 @@ namespace Ordering.Infrastructure
             var connectionString = configuration.GetConnectionString("Database");
 
             // Add Services to the container.
-            //services.AddDbContext<ApplicationDbContext>(options =>
-            //{
-            //    options.UseSqlServer(connectionString);
-            //});
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
 
-            //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            // services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
             return services;
         }
