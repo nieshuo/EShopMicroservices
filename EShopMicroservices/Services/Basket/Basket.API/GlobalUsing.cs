c91ae5a7ded317c3adbad90d386b1234e4010fdc
nieshuo <694891769@qq.com>
2024-07-12 17:08:31 +0800

    1丶完成部分rabbitmq代码

--- EShopMicroservices/Services/Basket/Basket.API/GlobalUsing.cs
+++ EShopMicroservices/Services/Basket/Basket.API/GlobalUsing.cs
@@ -8,6 +8,7 @@ global using System.Text.Json;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions;
 global using BuildingBlocks.Exceptions.Handler;
+global using BuildingBlocks.Messaging.MassTransit;
 global using Basket.API.Models;
 global using BuildingBlocks.CQRS;
 global using Basket.API.Data;
